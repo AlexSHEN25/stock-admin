7a908395504efcdfda2513ca46d73919d3cd9ffa
chinatatsu <shenalex25@gmail.com>
2026-03-31 10:54:36 +0900

    feat: add theme，sub menu，top navigator breadcrumbs

--- src/pages/user/login/index.tsx
+++ src/pages/user/login/index.tsx
@@ -1,28 +1,26 @@
 import { LoginForm, ProFormText } from '@ant-design/pro-components';
 import { message } from 'antd';
-import { getErrorMessage } from '@/services/http';
 import { login } from '@/services/user';
-import { t } from '@/utils/i18n';
 
 export default () => {
-  const handleSubmit = async (values: {
-    username: string;
-    password: string;
-  }) => {
+  const handleSubmit = async (values: any) => {
     try {
       const res = await login(values);
-      if (res.code === 200 && res.data?.token) {
-        localStorage.setItem('token', res.data.token);
-        localStorage.setItem('username', res.data.username || values.username);
-        message.success(res.message || t('login.success'));
-        window.location.href = window.location.pathname.endsWith('/user/login')
-          ? window.location.pathname.replace(/\/user\/login$/, '/')
-          : '/';
-        return;
+
+      if (res.code === 200) {
+        const { token, username } = res.data;
+
+        localStorage.setItem('token', token);
+        localStorage.setItem('username', username);
+
+        message.success(res.message || '登录成功');
+
+        window.location.href = '/';
+      } else {
+        message.error(res.message);
       }
-      message.error(res.message || t('login.failed'));
     } catch (e) {
-      message.error(getErrorMessage(e, t('login.exception')));
+      message.error('请求异常');
     }
   };
 
@@ -31,13 +29,13 @@ export default () => {
       <LoginForm onFinish={handleSubmit}>
         <ProFormText
           name="username"
-          placeholder={t('login.username')}
-          rules={[{ required: true, message: t('login.requiredUsername') }]}
+          placeholder="用户名"
+          rules={[{ required: true, message: '请输入用户名' }]}
         />
         <ProFormText.Password
           name="password"
-          placeholder={t('login.password')}
-          rules={[{ required: true, message: t('login.requiredPassword') }]}
+          placeholder="密码"
+          rules={[{ required: true, message: '请输入密码' }]}
         />
       </LoginForm>
     </div>
